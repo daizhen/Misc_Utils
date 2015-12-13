ad906194134539d39bc105cc1783587ba3cdfad3
daizhenbest <daizhenbest@gmail.com>
2015-12-13 22:32:09 +0800

    Update code

---
+++ SWS_Excel_Gather/ExcelGather/ExcelGather/RequestItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelGather
+{
+	public class RequestItem
+	{
+		public string RawName
+		{
+			get;
+			set;
+		}
+
+		public string FullName
+		{
+			get;
+			set;
+		}
+		public string SerialNum
+		{
+			get;
+			set;
+		}
+		public int Month
+		{
+			get;
+			set;
+		}
+
+		public double Money
+		{
+			get;
+			set;
+		}
+
+		public string FileName
+		{
+			get;
+			set;
+		}
+	}
+}
